7c7e11c1644903e37193b57bfce4ff9bd6a7fdde
tjmax79 <ogidantunji@gmail.com>
2025-11-04 11:48:04 +0100

    Add getComputerChoice function to script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+
+
+function getComputerChoice(){
+    const randomIndex = Math.trunc(Math.random() *3 )
+    if (randomIndex === 0) {
+        return ('Scissors');
+    }else if(randomIndex === 1){
+        return ('Rock');
+    }else{
+        return ('Paper');
+    }
+
+  
+}
+console.log(getComputerChoice());
